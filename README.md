98e5a38b06c299d2f42ef6e16b54d750d52789a8
Richtermnd <m.rustam1305@gmail.com>
2023-12-12 20:32:24 +0500

    Init.

---
+++ README.md
@@ -0,0 +1,3 @@
+### Gin CRUD Router
+
+It's just a part of code from one of my projects, that can be usefull for someone.
---
+++ pkg/crudRouter/crud.go
@@ -0,0 +1,92 @@
+package crud
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func getPk(ctx *gin.Context) string {
+	pk := ctx.Param("pk")
+	return strings.Trim(pk, "/")
+}
+
+// CRUDService Service that can make classic CRUD operations
+type CRUDService[T any] interface {
+	Create(T) (string, error)
+	Read(pk string) (*T, error)
+	ReadAll() ([]*T, error)
+	Update(pk string, updatedItem *T) (string, error)
+	Delete(pk string) (bool, error)
+}
+
+// CRUDRouter create router and add CRUD handlers if Service implement CRUDService interface.
+func CRUDRouter[T any](
+	apiRouter *gin.RouterGroup,
+	prefix string,
+	service CRUDService[T],
+) *gin.RouterGroup {
+	router := apiRouter.Group(prefix)
+	// Create
+	router.POST("/", func(ctx *gin.Context) {
+		var item T
+		if err := ctx.Bind(&item); err != nil {
+			ctx.AbortWithError(http.StatusBadRequest, err)
+			return
+		}
+		pk, err := service.Create(item)
+		if err != nil {
+			ctx.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		ctx.JSON(http.StatusOK, pk)
+	})
+
+	// ReadAll
+	router.GET("/", func(ctx *gin.Context) {
+		items, err := service.ReadAll()
+		if err != nil {
+			ctx.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		ctx.JSON(http.StatusOK, &items)
+	})
+
+	// Read
+	router.GET("/:pk", func(ctx *gin.Context) {
+		pk := getPk(ctx)
+		item, err := service.Read(pk)
+		if err != nil {
+			ctx.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		ctx.JSON(http.StatusOK, *item)
+	})
+
+	// Update
+	router.PUT("/:pk", func(ctx *gin.Context) {
+		// Getting item from body
+		var item T
+		if err := ctx.Bind(&item); err != nil {
+			ctx.AbortWithError(http.StatusBadRequest, err)
+			return
+		}
+		// Getting pk from params
+		pk := getPk(ctx)
+		// Updating item.
+		service.Update(pk, &item)
+	})
+
+	// Delete
+	router.DELETE("/:pk", func(ctx *gin.Context) {
+		pk := getPk(ctx)
+		res, err := service.Delete(pk)
+		if err != nil {
+			ctx.AbortWithError(http.StatusInternalServerError, err)
+		}
+		ctx.JSON(http.StatusOK, res)
+	})
+
+	return router
+}
